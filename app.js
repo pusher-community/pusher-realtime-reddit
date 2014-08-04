56fa63a4fd4288155663cf2f595dc3291f64aed4
Robin Hawkes <rob@rawkes.com>
2014-08-04 09:53:35 +0100

    Added Express and demo

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ try {
   }
 }
 
+var express = require("express");
+var bodyParser = require("body-parser");
+var errorHandler = require("errorhandler");
+
+var app = express();
+var root = __dirname + "/demo";
+
+
 // --------------------------------------------------------------------
 // SET UP PUSHER
 // --------------------------------------------------------------------
@@ -28,6 +36,37 @@ var pusher = new Pusher({
   key: config.pusher_key,
   secret: config.pusher_secret
 });
+
+
+// --------------------------------------------------------------------
+// SET UP EXPRESS
+// --------------------------------------------------------------------
+
+// Parse application/json and application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// Simple logger
+app.use(function(req, res, next){
+  console.log("%s %s", req.method, req.url);
+  console.log(req.body);
+  next();
+});
+
+// Error handler
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+// Serve static files from directory
+app.use(express.static(root));
+
+// Open server on specified port
+if (!silent) console.log("Starting Express server");
+app.listen(process.env.PORT || 5001);
 
 
 // --------------------------------------------------------------------
